bdd8732d6de0b33cb5ff23267c35b6881062419e
vladislvk <vladislaukim@gmail.com>
2024-02-11 00:19:52 +0600

    feat: added styling to the shadows

--- src/common/components/common-institution-card/institution-card.tsx
+++ src/common/components/common-institution-card/institution-card.tsx
@@ -18,6 +18,8 @@ export const InstituionCard = ({
 				<div className={styles.description}>{description}</div>
 				<div className={styles.review_wrapper}>
 					<div>{rating}</div>
+					{rating >= 7 ? "smile" : "sad"}
+
 					<div>{reviews}</div>
 					<div>dollasign</div>
 				</div>
@@ -25,3 +27,4 @@ export const InstituionCard = ({
 		</div>
 	)
 }
+// i need to add images for smile and sad
