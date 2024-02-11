07e9c1a5d39fc75284315691cac0f65ae1489d19
vladislav <137141831+vladislvk@users.noreply.github.com>
2024-02-11 14:40:17 +0600

    Revert "FEAT: creating and styling institution card"

--- src/common/components/common-institution-card/institution-card.types.ts
+++
@@ -1,7 +0,0 @@
-export interface InstitutionCardProps {
-	title: string
-	description: string
-	thumbnail: string
-	rating?: number
-	reviews: string
-}
--- src/modules/lending/components/main-banner/main-banner.tsx
+++ src/modules/lending/components/main-banner/main-banner.tsx
@@ -1,39 +1,26 @@
-import { Image } from "antd"
-import { SearchComponent } from "./components/search-component/search-component"
-import styles from "./main-banner.module.css"
-import { CarouselComponent } from "@/common/components/common-card-carousel"
-import { InstituionCard } from "@/common/components/common-institution-card/institution-card"
+import { Image } from 'antd'
+import { SearchComponent } from './components/search-component/search-component'
+import styles from './main-banner.module.css'
+import { CarouselComponent } from '@/common/components/common-card-carousel'
 
 export const MainBanner = () => {
-	return (
-		<aside className={styles.wrapper}>
-			<div className={styles.container}>
-				<div className={styles.left_side}>
-					<h1 className={styles.heading}>
-						Онлайн-поиск <br /> в сфере ментального <br /> здоровья
-					</h1>
-					<div className={styles.search}>
-						<span>Социальная поддержка помогает справиться с депрессией</span>
-						<SearchComponent />
-					</div>
-				</div>
-				<div className={styles.right_side}>
-					<img src="/lending.png" alt="Main" />
-				</div>
-			</div>
-			<CarouselComponent title={"Категории"} />
-
-			<div>
-				<InstituionCard
-					title={"1337 mental"}
-					description={"best place in the world"}
-					thumbnail={
-						"https://zapis.kz/data/pics/salon/pic-103764_small.jpg?v=0"
-					}
-					rating={"7.8"}
-					reviews={"ok"}
-				/>
-			</div>
-		</aside>
-	)
+    return (
+        <aside className={styles.wrapper}>
+            <div className={styles.container}>
+                <div className={styles.left_side}>
+                    <h1 className={styles.heading}>
+                        Онлайн-поиск <br/> в сфере ментального <br/> здоровья
+                    </h1>
+                    <div className={styles.search}>
+                        <span>Социальная поддержка помогает справиться с депрессией</span>
+                        <SearchComponent />
+                    </div>
+                </div>
+                <div className={styles.right_side}>
+                    <img src="/lending.png" alt="Main"/>
+                </div>
+            </div>
+            <CarouselComponent title={"Категории"} />
+        </aside>
+    )
 }
