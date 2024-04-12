6425e5f04bc56f00cb5d0358f1f86f4eb9015b37
vladislvk <vladislaukim@gmail.com>
2024-04-13 00:28:11 +0500

    FEAT: create curator cabinet and documenatation page

--- src/common/api/responses/student.response.ts
+++ src/common/api/responses/student.response.ts
@@ -1,12 +1,12 @@
 export interface StudentResponse {
-    id: string;
-    name: string | null;
-    surname: string;
-    state: number;
-    grade: string;
-    description: string
-    created_at: string;
-    updatedAt: string;
-    deletedAt: string;
+	id: string
+	name: string | null
+	surname: string
+	state: number
+	grade: string
+	description: string
+	performance: number
+	created_at: string
+	updatedAt: string
+	deletedAt: string
 }
-  
